7123316286c0117aa80cf60e920cde6857cd5046
DJboicool <147611625+DJboicool@users.noreply.github.com>
2023-10-11 20:31:39 +0530

    Update New Text Document.php

---
+++ New Text Document.php
@@ -0,0 +1,24 @@
+<?php
+session_start(); // Start or resume the session
+
+// Sample user credentials. Replace this with a database query in a real application.
+$users = [
+    'djproject' => 'djboicool',
+    'gsus' => 'gsusgay',
+    'momedeh' => 'idkwut',
+];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if (array_key_exists($username, $users) && $users[$username] === $password) {
+        // Authentication successful
+        $_SESSION['username'] = $username;
+        header("Location: restricted_page.php"); // Redirect to the restricted page
+    } else {
+        // Authentication failed
+        header("Location: login.html?login=failed"); // Redirect to the sign-in page with an error message
+    }
+}
+?>
